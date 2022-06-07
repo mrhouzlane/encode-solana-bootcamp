164ad78756fb626a9c62eb124009f916068c1ebf
mrhouzlane <mrhouzlane@github.com>
2022-06-07 17:03:08 +0200

    reactTemplate

--- programs/tests/russian_roulette.ts
+++ programs/tests/russian_roulette.ts
@@ -5,33 +5,37 @@ import { RussianRoulette } from "../target/types/russian_roulette";
 
 const LAMPORTS_PER_SOL = 1_000_000_000;
 
+
+const provider = anchor.AnchorProvider.env();
+anchor.setProvider(provider);
+
+
 describe("russian_rulette", () => {
   // Configure the client to use the local cluster.
-
-  const provider = anchor.AnchorProvider.env();
-  anchor.setProvider(provider);
-
   const program = anchor.workspace.RussianRulette as Program<RussianRoulette>;
+  const user = provider.wallet;
 
-  it("Is initialized!", async () => {
-    
+  it("Initialized!", async () => {
+
+    console.log("Test print");
     // Add your test here.
+    //const tx = await program.methods.execute("").rpc();
     const signer_keypair: Keypair = anchor.web3.Keypair.generate();
     let russian_roulette_key: PublicKey;
 
     // Give SOL to our account
-    airdrop(signer_keypair.publicKey, 100);
-    
-    //let create_game_tx = await program.methods.createGame(new anchor.BN(2))
-    //            .signers([signer_keypair]).rpc();
-
-    // console.log("Your transaction signature", create_game_tx);
-  });
-  
-  async function airdrop(requester: PublicKey, amount: number) {
     await provider.connection.confirmTransaction(
-      await provider.connection.requestAirdrop(requester, amount * LAMPORTS_PER_SOL)
+      await provider.connection.requestAirdrop(signer_keypair.publicKey, 100 * LAMPORTS_PER_SOL)
     );
-  }
+    const program = anchor.workspace.RussianRulette as Program<RussianRoulette>;
+    console.log("Test end");
+
+    //Create game test : 
+    const ticket_price = 20;
+    let create_game_tx = await program.methods.createGame(new anchor.BN(2)).signers([signer_keypair]).rpc();
+
+    });
+    // russian_roulette.ticket_price = ticket_price;
 
 });
+
